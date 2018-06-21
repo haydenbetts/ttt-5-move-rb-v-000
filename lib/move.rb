87c43810d6651023dc072971c53047b4931afb53
Hayden Betts <haydenbetts@gmail.com>
2018-06-21 00:09:54 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board_array, index, value)
+def move(board_array, index, player_token)
   # update the TTT board with the player's token
 end
